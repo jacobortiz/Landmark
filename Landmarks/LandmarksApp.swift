98dcebc8af0fa1fa87fb285e2f5d6a634a493cbd
Jacob Ortiz <jacobortiz90@gmail.com>
2024-07-19 11:21:21 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jacob Ortiz on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
